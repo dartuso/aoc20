461d59aec15a2c153f1db0ed7ab2036a2428513e
Daniel <daniel.artuso1@gmail.com>
2020-12-09 12:37:12 -0700

    day1part2

---
+++ day1part2.js
@@ -0,0 +1,33 @@
+const file = "day1.txt"
+const year = 2020;
+const fs = require("fs");
+
+function sortNumbers(a, b) {
+    return a - b;
+}
+
+function findNumbers() {
+    let a = 0, b = 0, c = 0, product = 0;
+    let number = fs.readFileSync(file).toString('utf-8').split('\r\n').map(x => {
+        return parseInt(x)
+    });
+    number.sort(sortNumbers)
+    let length = number.length
+
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            for (let k = j + 1; k < length; k++) {
+                if (year === (number[i] + number[j] + number[k])) {
+                    a = number[i];
+                    b = number[j];
+                    c = number[k];
+                    product = a * b * c;
+                }
+            }
+        }
+    }
+    console.log(a + b + c)
+    return [a, b, c, product];
+}
+
+console.log(findNumbers())
