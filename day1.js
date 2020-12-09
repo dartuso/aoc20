a58967d5164c44d6c5f1c355f019de0a46afb5e4
Daniel <daniel.artuso1@gmail.com>
2020-12-09 13:44:19 -0700

    increased efficency

--- day1.js
+++ day1.js
@@ -2,15 +2,31 @@ const file = "day1.txt"
 const year = 2020;
 const fs = require("fs");
 
-function findDay1(){
-    let a = 0, b = 0, product = 0;
+function findDay1() {
     const number = fs.readFileSync(file).toString('utf-8').split('\r\n').map(x => {
         return parseInt(x)
     });
+    let set = new Set();
+    for (let i = 0; i < number.length; i++) {
+        let temp = year - number[i];
+        if (set.has(temp)) {
+            let product = temp * number[i];
+            const answer = [temp, number[i], product];
+            console.log(answer);
+            return answer;
+        } else {
+            set.add(number[i])
+        }
+    }
+}
 
+
+module.exports = findDay1;
+
+/* slow way to find - O(n^2)?
     for (let i = 0; i < number.length; i++) {
         for (let j = i + 1; j < number.length; j++) {
-            if(year === (number[i] + number[j])){
+            if (year === (number[i] + number[j])) {
                 a = number[i];
                 b = number[j];
                 product = a * b;
@@ -18,8 +34,4 @@ function findDay1(){
             }
         }
     }
-    return [a,b,product];
-}
-console.log(findDay1())
-
-module.exports = findDay1;
+    return [a, b, product];*/
--- day1part2.js
+++ day1part2.js
@@ -3,32 +3,30 @@ const year = 2020;
 const fs = require("fs");
 
 function findDay1Part2() {
-    let a = 0, b = 0, c = 0, product = 0;
+    let product = 0, answer = [];
     let number = fs.readFileSync(file).toString('utf-8').split('\r\n').map(x => {
         return parseInt(x)
-    }).sort(function(a, b){return a-b})
+    }).sort(function (a, b) {
+        return a - b
+    })
     let length = number.length
-
 
     for (let i = 0, l, r; i < length; i++) {
         l = i + 1;
         r = length - 1;
         while (l < r) {
             if (number[i] + number[l] + number[r] === year) {
-                a = number[i];
-                b = number[l];
-                c = number[r];
-                product = a * b * c;
-                break;
-            }
-            else if (number[i] + number[l] + number[r] < year)
+                product = number[i] * number[l] * number[r];
+                answer = [number[i],number[l],number[r],product]
+                console.log(answer)
+                return answer;
+            } else if (number[i] + number[l] + number[r] < year)
                 l++;
-            else // A[i] + A[l] + A[r] > sum
+            else // sum > year
                 r--;
         }
     }
-        return [a, b, c, product];
+
 }
-console.log(findDay1Part2())
 
 module.exports = findDay1Part2;
